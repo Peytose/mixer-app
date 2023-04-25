dd32bb907318f1de7cd25db231ec360c5a4b8cfe
Peytose <jose.martinez102001@gmail.com>
2023-04-25 17:21:39 -0400

    Worked on search page UI

--- mixer/View/Search/SearchView.swift
+++ mixer/View/Search/SearchView.swift
@@ -11,59 +11,103 @@ import DebouncedOnChange
 
 struct SearchView: View {
     @ObservedObject var viewModel = SearchViewModel()
+    @State var showAlert = false
     
     var body: some View {
         NavigationView {
             ZStack {
-                List {
-                    ForEach(viewModel.users) { user in
-                        NavigationLink {
-                            ProfileView(viewModel: ProfileViewModel(user: user))
-                        } label: {
-                            UserSearchCell(username: user.username,
-                                           name: user.name,
-                                           imageUrl: user.profileImageUrl)
-                        }
+                VStack {
+                    List {
+                        listContent
                     }
+                    .scrollContentBackground(.hidden)
                 }
-                .scrollContentBackground(.hidden)
-                
+                .background(Color.mixerBackground)
+                .overlay {
+                    Image("Blob 1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 300, height: 300, alignment: .top)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                        .opacity(0.8)
+                        .offset(x: -40, y: -355)
+                }
                 if viewModel.isLoading { LoadingView() }
             }
+            .navigationTitle("Search")
             .searchable(text: $viewModel.text,
                         placement: .automatic,
-                        prompt: "Add friends!")
-            .onChange(of: viewModel.text, debounceTime: 0.8) { search in
-                viewModel.executeSearch(for: search)
+                        prompt: "Search Users")
+            .onChange(of: viewModel.text) { search in
+                viewModel.executeSearch(for: search.lowercased())
             }
         }
-        .background(Color.mixerBackground.edgesIgnoringSafeArea(.all))
+        .preferredColorScheme(.dark)
     }
 }
 
-fileprivate struct UserSearchCell: View {
-    let username: String
-    let name: String
-    let imageUrl: String
-    
-    var body: some View {
-        HStack(alignment: .center, spacing: 10) {
-            KFImage(URL(string: imageUrl))
-                .resizable()
-                .scaledToFill()
-                .clipShape(Circle())
-                .frame(width: 37, height: 37)
-            
-            VStack(alignment: .leading) {
-                Text(name)
-                    .font(.headline)
-                    .foregroundColor(.white)
-                
-                Text("@\(username)")
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
+extension SearchView {
+    var listContent: some View {
+        ForEach(viewModel.users) { user in
+            NavigationLink(destination: ProfileView(viewModel: ProfileViewModel(user: user))) {
+                UserSearchCell(user: user)
+            }
+            .listRowSeparator(.hidden)
+            .listRowBackground(Color.mixerBackground)
+            .swipeActions {
+                Button("Add Friend") {
+                    showAlert.toggle()
+                }
+                .tint(Color.mixerPurple)
             }
         }
+        .alert("Friend Request Sent", isPresented: $showAlert) {
+            Button("Ok", role: .cancel) { }
+        }    }
+}
+
+fileprivate struct UserSearchCell: View {
+    let user: CachedUser
+    
+    var body: some View {
+        HStack(spacing: 15) {
+            KFImage(URL(string: user.profileImageUrl))
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 45, height: 45)
+                .clipShape(Circle())
+            
+            VStack(alignment: .leading) {
+                HStack(spacing: 2) {
+                    Text(user.name)
+                        .font(.callout.weight(.semibold))
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "graduationcap.fill")
+                        .imageScale(.small)
+                        .foregroundColor(.secondary)
+                    
+                    Text("\(user.university)")
+                        .font(.subheadline.weight(.medium))
+                        .foregroundColor(.secondary)
+                }
+                
+                HStack {
+                    Text("@\(user.username)")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary.opacity(0.7))
+                    
+                    Spacer()
+                    
+                    Text("Friend")
+                        .font(.subheadline)
+                        .foregroundColor(.mixerIndigo)
+                }
+            }
+        }
+        .padding(.vertical, -4)
     }
 }
 
