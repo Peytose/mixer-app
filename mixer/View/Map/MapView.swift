f66144b16fbe2351d973ae55c5b99b838fc0d69f
Peyton Lyons <peyton.lyons@outlook.com>
2023-04-28 02:23:14 -0400

    create event is full screen and map view rename

--- mixer/View/Map/MapView.swift
+++ mixer/View/Map/MapView.swift
@@ -1,130 +1,193 @@
-////
-////  MapView.swift
-////  mixer
-////
-////  Created by Peyton Lyons on 11/12/22.
-////
 //
-//import SwiftUI
-//import UIKit
-//import MapKit
-//import CoreLocationUI
+//  MapView.swift
+//  mixer
 //
-//struct MapView: View {
-//    @Namespace var namespace
-//    @StateObject private var viewModel = LocationMapViewModel()
-//    
-//    @State var showGuestListView = false
-//    
-//    var body: some View {
-//        ZStack(alignment: .top) {
-//            MapClusterView()
-//                .ignoresSafeArea()
-//            
-//            VStack(spacing: 10) {
-//                EventUsersListButton(action: $showGuestListView)
-//            }
-//            .frame(maxWidth:.infinity, maxHeight: .infinity, alignment: .topTrailing)
-//            .padding(EdgeInsets(top: 60, leading: 0, bottom: 0, trailing: 6))
-//            
-//            BlurredStatusBar()
-//        }
-//        .overlay(alignment: .bottomTrailing, content: {
-//            LocationButton(.currentLocation) {
-//                withAnimation() {
-//                }
-//            }
-//            .foregroundColor(Color.mainFont)
-//            .symbolVariant(.fill)
-//            .tint(Color.mixerSecondaryBackground)
-//            .labelStyle(.iconOnly)
-//            .clipShape(Circle())
-//            .shadow(radius: 5, y: 10)
-//            .padding(EdgeInsets(top: 0, leading: 0, bottom: 90, trailing: 6))
-//        })
-//        .overlay(alignment: .bottom, content: {
-//            UserQRCodeButton()
-//                .onTapGesture {
-//                    withAnimation() {
-//                        viewModel.isShowingQRCodeView.toggle()
-//                    }
-//                }
-//                .padding(EdgeInsets(top: 0, leading: 0, bottom: 90, trailing: 20))
-//                .onTapGesture {
-//                    let impact = UIImpactFeedbackGenerator(style: .medium)
-//                    impact.impactOccurred()
-//                    
-//                    viewModel.isShowingAddEventView.toggle()
-//                }
-//        })
-////        .sheet(isPresented: $showGuestListView, content: {
-////            GuestListView()
-////        })
-//    }
-//}
+//  Created by Peyton Lyons on 2/4/23.
 //
-//
-//struct MapView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        MapView()
-//            .preferredColorScheme(.dark)
-//    }
-//}
-//
-//fileprivate struct EventUsersListButton: View {
-//    @Binding var action: Bool
-//    var body: some View {
-//        Image(systemName: "list.clipboard")
-//            .font(.title2.weight(.medium))
-//            .foregroundColor(Color.mainFont)
-//            .padding(10)
-//            .background(Color.mixerSecondaryBackground)
-//            .clipShape(Circle())
-//            .shadow(radius: 5, y: 8)
-//            .onTapGesture {
-//                let impact = UIImpactFeedbackGenerator(style: .light)
-//                impact.impactOccurred()
-//                
-//                action.toggle()
-//            }
-//    }
-//}
-//
-//
-//fileprivate struct UserQRCodeButton: View {
-//    var body: some View {
-//        Rectangle()
-//            .fill(Color.mixerPurpleGradient)
-//            .cornerRadius(30)
-//            .frame(width: 150, height: 50)
-//            .shadow(radius: 5, y: 10)
-//            .overlay(content: {
-//                HStack(spacing: 15) {
-//                    Image(systemName: "qrcode")
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fit)
-//                        .frame(width: 26, height: 26)
-//                        .foregroundColor(Color.white)
-//                }
-//            })
-//    }
-//}
-//
-//
-//struct BlurredStatusBar: View {
-//    var height: CGFloat = 50
-//    var color: Color = Color.clear
-//    var blurRadius: CGFloat = 5
-//    var body: some View {
-//        VStack {
-//            Rectangle()
-//                .fill(color)
-//                .frame(maxWidth: .infinity, maxHeight: height, alignment: .top)
-//                .backgroundBlur(radius: blurRadius, opaque: true)
-//                .blur(radius: 2)
-//                .ignoresSafeArea()
-//            
-//            Spacer()
-//        }
-//    }
-//}
+
+import SwiftUI
+import MapKit
+import CoreLocationUI
+
+struct MapView: View {
+    @ObservedObject var viewModel = MapViewModel()
+    @State var isShowingDetailView: Bool      = false
+    @State var isShowingGuestlistView: Bool   = false
+    @State var isShowingCreateEventView: Bool = false
+    @State private var selectedEvent: CachedEvent?
+    @State private var selectedHost: CachedHost?
+    @State private var progress: CGFloat = 0
+    let gradient1 = Gradient(colors: [.purple, .yellow])
+    let gradient2 = Gradient(colors: [.blue, .purple])
+    var namespace: Namespace.ID
+    
+    var body: some View {
+        ZStack(alignment: .top) {
+            Map(coordinateRegion: $viewModel.region,
+                showsUserLocation: true,
+                annotationItems: viewModel.mapItems.keys.compactMap { $0 }) { host in
+                MapAnnotation(coordinate: host.location?.locationCoordinate ?? CLLocationCoordinate2D(latitude: 42.3598, longitude: 71.0921),
+                              anchorPoint: CGPoint(x: 0.75, y: 0.75)) {
+                    HostMapAnnotation(host: host)
+                        .onTapGesture {
+                            if let event = viewModel.mapItems[host] as? CachedEvent {
+                                self.selectedEvent = event
+                            } else {
+                                self.selectedHost = host
+                            }
+                            
+                            isShowingDetailView = true
+                        }
+                }
+            }
+            .ignoresSafeArea()
+            
+            Spacer()
+            
+            LogoView(frameWidth: 75)
+                .animatableGradient(fromGradient: gradient1,
+                                    toGradient: gradient2,
+                                    progress: progress)
+                .frame(height: 75)
+                .mask(LogoView(frameWidth: 75))
+                .shadow(radius: 10)
+                .allowsHitTesting(false)
+                .onAppear {
+                    withAnimation(.linear(duration: 5.0).repeatForever(autoreverses: true)) {
+                        self.progress = 1.0
+                    }
+                }
+        }
+        .sheet(isPresented: $isShowingDetailView) {
+            if let event = selectedEvent {
+                NavigationView {
+                    EventDetailView(viewModel: EventDetailViewModel(event: event),
+                                    namespace: namespace)
+                    .toolbar {
+                        ToolbarItem(placement: .destructiveAction) {
+                            Button { isShowingDetailView = false  } label: { XDismissButton() }
+                        }
+                    }
+                }
+            } else if let host = selectedHost {
+                NavigationView {
+                    HostDetailView(viewModel: HostDetailViewModel(host: host), namespace: namespace)
+                        .toolbar {
+                            ToolbarItem(placement: .destructiveAction) {
+                                Button { isShowingDetailView = false  } label: { XDismissButton() }
+                            }
+                        }
+                }
+            }
+        }
+        .fullScreenCover(isPresented: $isShowingGuestlistView) {
+            if let event = viewModel.hostEvents.first?.value, !viewModel.hostEvents.isEmpty {
+                GuestlistView(viewModel: GuestlistViewModel(event: event), isShowingGuestlistView: $isShowingGuestlistView)
+                    .zIndex(2)
+            }
+        }
+        .fullScreenCover(isPresented: $isShowingCreateEventView) {
+            CreateEventFlow(isShowingCreateEventView: $isShowingCreateEventView)
+        }
+        .overlay(alignment: .topTrailing) {
+            if let isHost = AuthViewModel.shared.currentUser?.isHost {
+                MapIconButton(icon: "plus", hasLargerSize: isHost) { isShowingCreateEventView.toggle() }
+                    .padding(.trailing)
+                    .padding(.top)
+            }
+        }
+        .overlay(alignment: .bottom, content: {
+            if let _ = AuthViewModel.shared.currentUser?.isHost {
+                GuestlistButton { isShowingGuestlistView.toggle() }
+                    .padding(.bottom, 100)
+            }
+        })
+        .overlay(alignment: .bottomTrailing) {
+            Button {
+                let impact = UIImpactFeedbackGenerator(style: .light)
+                impact.impactOccurred()
+                viewModel.requestAlwaysOnLocationPermission()
+            } label: {
+                Image("recenter-button")
+                    .renderingMode(.template)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .foregroundColor(Color.mainFont)
+                    .frame(width: 25, height: 25)
+                    .padding(10)
+                    .background(Color.mixerSecondaryBackground)
+                    .clipShape(Circle())
+                    .shadow(radius: 5, y: 8)
+            }
+            .padding(.trailing)
+            .padding(.bottom, 180)
+        }
+        .alert(item: $viewModel.alertItem, content: { $0.alert })
+        .task {
+            viewModel.getMapItems()
+            viewModel.getEventForGuestlist()
+        }
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    @Namespace static var namespace
+    
+    static var previews: some View {
+        MapView(namespace: namespace)
+    }
+}
+
+fileprivate struct MapIconButton: View {
+    let icon: String
+    let hasLargerSize: Bool
+    let action: () -> Void
+    
+    var body: some View {
+        Button {
+            let impact = UIImpactFeedbackGenerator(style: .light)
+            impact.impactOccurred()
+            
+            action()
+        } label: {
+            Image(systemName: icon)
+                .font(hasLargerSize ? .title2 : .title3)
+                .fontWeight(.medium)
+                .foregroundColor(Color.mainFont)
+                .padding(hasLargerSize ? 15 : 10)
+                .background(Color.mixerSecondaryBackground)
+                .clipShape(Circle())
+                .shadow(radius: 5, y: 8)
+        }
+    }
+}
+
+fileprivate struct GuestlistButton: View {
+    let action: () -> Void
+    
+    var body: some View {
+        Button {
+            let impact = UIImpactFeedbackGenerator(style: .light)
+            impact.impactOccurred()
+            action()
+        } label: {
+            Capsule()
+                .fill(Color.mixerSecondaryBackground)
+                .frame(width: DeviceTypes.ScreenSize.width * 0.9, height: 55)
+                .shadow(radius: 20, x: -8, y: -8)
+                .shadow(radius: 20, x: 8, y: 8)
+                .overlay {
+                    HStack {
+                        Image(systemName: "list.clipboard")
+                            .imageScale(.large)
+                            .foregroundColor(.mainFont)
+                        
+                        Text("Guestlist")
+                            .font(.body.weight(.medium))
+                            .foregroundColor(.white)
+                    }
+                }
+                .shadow(radius: 5, y: 10)
+        }
+    }
+}
